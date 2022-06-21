6313d11ae9f02329ddb6228bc8598b9e65699fd9
AhmedRAlsawi <ahmedraafatal.sawi@gmail.com>
2022-06-21 14:36:09 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='container'>
+        <h2 className='w-100 text-center my-2 border bg-warning text-white py-2'>
+          Log in form
+        </h2>
+        <form className='w-50 bg-danger mx-auto'>
+          <div className='row'>
+            <section className='mb-2'>
+              <label className='w-100'>
+                Username
+              </label>
+              <input type='text' id='username-input'></input>
+            </section>
+          </div>
+          <div className='row'>
+            <section className='mb-2'>
+              <label className='w-100'>
+                Password
+              </label>
+              <input type='password' id='password-input'></input>
+            </section>
+          </div>
+          <footer className='mb-2'>
+            <button type='submit' id='login-button'>Log in</button>
+          </footer>
+        </form>
+
+      </section>
+    </>
   );
 }
 
